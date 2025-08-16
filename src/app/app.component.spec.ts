2fed36b5b7ea42b740ec6da0ed4f772cd5d41f8f
tbirangi <tbirangi@gmail.com>
2025-08-15 23:38:28 -0500

    fixed title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pricingmodel'`, () => {
+  it(`should have as title 'pricingmodule'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pricingmodel');
+    expect(app.title).toEqual('pricingmodule');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pricingmodel app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pricingmodule app is running!');
   });
 });
